9c115327a10a6d5fd503a73f842db95a2eb36668
Jessica Chou <j4chou@gmail.com>
2017-02-11 15:23:09 -0800

    Add projects page

--- server.js
+++ server.js
@@ -11,48 +11,52 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
 
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) console.log('Unable to append to server.log.');
-  });
-  next();
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) console.log('Unable to append to server.log.');
+    });
+    next();
 });
 
 app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-})
+    res.render('maintenance.hbs')
+});
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+    return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
+    return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome home!'
-  })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome home!'
+    });
 });
- 
+
 app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page',
-   });
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('project.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on port: ${port}`);
+    console.log(`Server listening on port: ${port}`);
 });
-
